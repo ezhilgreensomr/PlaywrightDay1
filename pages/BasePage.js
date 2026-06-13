7a3c806a3f9095def7660f7e2774f787c04c6de7
ezhilgreensomr <147533004+ezhilgreensomr@users.noreply.github.com>
2026-06-13 11:12:30 +0530

    Update BasePage.js

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -1,27 +1,27 @@
-export class BasePage{
-    constructor(page){
+// export class BasePage{
+//     constructor(page){
 
-        this.page = page;
-    }
-    //fill()
-    async enterText(locator,text){
+//         this.page = page;
+//     }
+//     //fill()
+//     async enterText(locator,text){
 
-        await locator.fill(text);
-    }
-    //click()
-    async btnClick(locator){
+//         await locator.fill(text);
+//     }
+//     //click()
+//     async btnClick(locator){
 
-        await locator.click();
-    }
-    //screenshot()
-    async takeScreenshot(location){
+//         await locator.click();
+//     }
+//     //screenshot()
+//     async takeScreenshot(location){
 
-        await this.page.screenshot({path:location, fullpage: true});
-    }
+//         await this.page.screenshot({path:location, fullpage: true});
+//     }
 
-    //waitForTimeout()
-    async waitForSeconds(seconds){
+//     //waitForTimeout()
+//     async waitForSeconds(seconds){
 
-        await this.page.waitForTimeout(seconds * 1000);
-    }
-}
+//         await this.page.waitForTimeout(seconds * 1000);
+//     }
+// }
