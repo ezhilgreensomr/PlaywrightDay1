81352e14154ec972bbf178799a6f194e50c7deac
ezhilgreensomr <147533004+ezhilgreensomr@users.noreply.github.com>
2026-06-13 11:12:14 +0530

    Update loginpage.js

--- pages/loginpage.js
+++ pages/loginpage.js
@@ -1,57 +1,57 @@
-import { BasePage } from "./BasePage.js";
+// import { BasePage } from "./BasePage.js";
 
-//page class 
-export class LoginPage extends BasePage{
+// //page class 
+// export class LoginPage extends BasePage{
 
-    constructor(page){
+//     constructor(page){
 
-        super(page);
-        this.page = page;
-    }
-    username(){
-        let email = this.page.locator("[name='email']");
+//         super(page);
+//         this.page = page;
+//     }
+//     username(){
+//         let email = this.page.locator("[name='email']");
 
-        return email;
-    }
-    password(){
+//         return email;
+//     }
+//     password(){
 
-        let pass = this.page.locator("[name='pass']");
-        return pass;
-    }
-    loginbtn(){
+//         let pass = this.page.locator("[name='pass']");
+//         return pass;
+//     }
+//     loginbtn(){
 
-        let loginbtn = this.page.locator("//span[text()='Log in']");
-        return loginbtn;
-    }
+//         let loginbtn = this.page.locator("//span[text()='Log in']");
+//         return loginbtn;
+//     }
 
-    async enterUsername(username){
+//     async enterUsername(username){
 
-       await this.enterText(this.username(),username);
-    }
+//        await this.enterText(this.username(),username);
+//     }
 
-    async enterPassword(password){
+//     async enterPassword(password){
 
-        await this.enterText(this.password(),password);
+//         await this.enterText(this.password(),password);
 
-    }
-    async clickLoginBtn(){
+//     }
+//     async clickLoginBtn(){
 
-        await this.btnClick(this.loginbtn());
-    }
+//         await this.btnClick(this.loginbtn());
+//     }
 
-    async goToLoginpage(){
+//     async goToLoginpage(){
 
-        await this.page.goto("https://www.facebook.com/");
-    }
+//         await this.page.goto("https://www.facebook.com/");
+//     }
 
-    async login(username,password){
+//     async login(username,password){
 
-        // await this.username().fill(username);
-        // await this.password().fill(password);
-        // await this.loginbtn().click();
+//         // await this.username().fill(username);
+//         // await this.password().fill(password);
+//         // await this.loginbtn().click();
 
-        await this.enterText(this.username(),username);
-        await this.enterText(this.password(), password);
-        await this.btnClick(this.loginbtn());
-    }
-}
+//         await this.enterText(this.username(),username);
+//         await this.enterText(this.password(), password);
+//         await this.btnClick(this.loginbtn());
+//     }
+// }
